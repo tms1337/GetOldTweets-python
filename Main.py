ccd5d221928e374099af42b487fc4f856168f4e2
Your Name <you@example.com>
2017-10-19 23:48:45 +0200

    remove unnecessary parts

--- Main.py
+++ Main.py
@@ -1,36 +1,22 @@
 import sys
+
 if sys.version_info[0] < 3:
     import got
 else:
     import got3 as got
 
+
 def main():
+    tweetCriteria = got.manager.TweetCriteria().setQuerySearch('bitcoin').setSince("2017-10-17").setUntil(
+        "2017-10-18")
+    tweets = got.manager.TweetManager.getTweets(tweetCriteria)
 
-	def printTweet(descr, t):
-		print(descr)
-		print("Username: %s" % t.username)
-		print("Retweets: %d" % t.retweets)
-		print("Text: %s" % t.text)
-		print("Mentions: %s" % t.mentions)
-		print("Hashtags: %s\n" % t.hashtags)
+    print(len(tweets))
 
-	# Example 1 - Get tweets by username
-	tweetCriteria = got.manager.TweetCriteria().setUsername('barackobama').setMaxTweets(1)
-	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
+    # for t in tweets:
+    #     if t.favorites > 0:
+    #         print(t)
 
-	printTweet("### Example 1 - Get tweets by username [barackobama]", tweet)
-
-	# Example 2 - Get tweets by query search
-	tweetCriteria = got.manager.TweetCriteria().setQuerySearch('europe refugees').setSince("2015-05-01").setUntil("2015-09-30").setMaxTweets(1)
-	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-
-	printTweet("### Example 2 - Get tweets by query search [europe refugees]", tweet)
-
-	# Example 3 - Get tweets by username and bound dates
-	tweetCriteria = got.manager.TweetCriteria().setUsername("barackobama").setSince("2015-09-10").setUntil("2015-09-12").setMaxTweets(1)
-	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-
-	printTweet("### Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']", tweet)
 
 if __name__ == '__main__':
-	main()
+    main()
--- got/models/Tweet.py
+++ got/models/Tweet.py
@@ -1,4 +1,13 @@
 class Tweet:
-	
-	def __init__(self):
-		pass
+    def __str__(self):
+        my_dict = self._to_dict()
+        rows = [('%s->%s' % (k, v)).encode('utf-8') for k, v in my_dict.items()]
+
+        return '\n'.join(rows)
+
+    def _to_dict(self):
+        d = {}
+        for attr, value in self.__dict__.iteritems():
+            d[attr] = value
+
+        return d
